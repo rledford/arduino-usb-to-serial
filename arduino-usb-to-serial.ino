15ecd64c6715530c6aabe921fe85050c2974b1d0
rledford <ryan.c.ledford@gmail.com>
2017-05-22 13:27:33 -0500

    initial commit

---
+++ arduino-usb-to-serial.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(19200);
+  Serial1.begin(19200);
+}
+
+void loop() {
+  if (Serial.available()) {
+    Serial1.write(Serial.read());
+  }
+  if (Serial1.available()) {
+    Serial.write(Serial1.read());
+  }
+}
